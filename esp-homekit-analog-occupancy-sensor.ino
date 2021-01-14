b2326ad5706851fe77c4daba4c8fff65409930bf
danielhelmstedt <38815163+danielhelmstedt@users.noreply.github.com>
2021-01-15 06:02:05 +1000

    Tidy up

--- esp-homekit-analog-occupancy-sensor.ino
+++ esp-homekit-analog-occupancy-sensor.ino
@@ -1,12 +1,5 @@
-///// CHANGELOG
-//0.2 added WifiManager Library
-//0.3 added ArduinoOTA
-//0.4 added blink when identifying during Homekit setup
-//0.5 added sensorValue RO char and threshold RW char.
-//0.6 Average/Filter ADC readings
-//0.7 Dynamic Serial/Hostname/AP based off ESP.getChipID()
-//0.8 Save threshold to EEPROM.
-
+//esp-homekit-analog-occupancy-sensor.ino
+//ESP8266-based, native HomeKit pressure sensing device via ADC reading and capaticive/pressure sensitive pads
 //TODO: Dynamic Homekit Name based off ESP.getChipID()
 
 #include <ArduinoOTA.h>
@@ -21,20 +14,21 @@
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_occupancy;
+extern "C" homekit_characteristic_t cha_sensorValue;
+extern "C" homekit_characteristic_t cha_threshold;
+
 #define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
 
 static uint32_t next_heap_millis = 0;
 static uint32_t next_report_millis = 0;
 
-extern "C" homekit_server_config_t config;
-extern "C" homekit_characteristic_t cha_occupancy;
-extern "C" homekit_characteristic_t cha_sensorValue;
-extern "C" homekit_characteristic_t cha_threshold;
- 
+//Data to save to EEPROM
 struct { 
   int eepromThreshold;
 } data;
-uint addr = 0;   //Location we want the threshold to be put in EEPROM.
+uint addr = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -50,9 +44,9 @@ void setup() {
   EEPROM.begin(512);  //Initialize EEPROM
   EEPROM.get(addr, data);
   Serial.print("Reading saved EEPROM Threshold - ");
-  Serial.println(String(data.eepromThreshold));
+  Serial.println(data.eepromThreshold);
   
-  pinMode(LED_BUILTIN, OUTPUT); //Initialize built-in LED
+  pinMode(LED_BUILTIN, OUTPUT);    //Initialize built-in LED
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED off (Active Low)
   
   WiFiManager wifiManager;
@@ -110,7 +104,11 @@ void homekit_loop() {
   }
 }
 
-//Function for reading sensor and reporting to Homekit. 
+
+/////////////////////////////////////////////////////////////////////
+////////Function for reading sensor and reporting to HomeKit.////////
+/////////////////////////////////////////////////////////////////////
+
 void homekit_report() {
   int numReadings = 10;
   int total = 0;
@@ -119,48 +117,35 @@ void homekit_report() {
 
   // Take 10 readings and average them
   for (int i = 0; i <= numReadings; i++) {
-    reading = analogRead(A0); // read from the sensor:
-    total = total + reading; // add the reading to the total:
-  }
+    reading = analogRead(A0);     // read from the sensor:
+    total = total + reading; }    // add the reading to the total:
   average = total / numReadings;  // calculate the average:
-  cha_sensorValue.value.int_value = average;
   
-
-  if(cha_threshold.value.int_value == 0 && data.eepromThreshold != 0) { //If homekit threshold is 0 and EEPROM has data saved
+  //Initialize HomeKit value from EEPROM on boot
+  if(cha_threshold.value.int_value == 0 && data.eepromThreshold != 0) { 
     cha_threshold.value.int_value = data.eepromThreshold;
     homekit_characteristic_notify(&cha_threshold, cha_threshold.value);
-    Serial.println("Updated Homekit threshold with saved value");
+    Serial.println("Updated HomeKit with saved value");
   }
   
-  cha_occupancy.value.bool_value = average <= cha_threshold.value.int_value ? 0 : 1; //Logic using Threshold
+  //Update HomeKit
+  cha_sensorValue.value.int_value = average;
+  cha_occupancy.value.bool_value = average <= cha_threshold.value.int_value ? 0 : 1; //Determine occupancy status
   homekit_characteristic_notify(&cha_occupancy, cha_occupancy.value);
   homekit_characteristic_notify(&cha_sensorValue, cha_sensorValue.value);
-  
-  Serial.print("sensor average = ");
-  Serial.println(average);
   Serial.print("threshold = ");
   Serial.println(cha_threshold.value.int_value);
+  Serial.print("sensor average = ");
+  Serial.println(average)
   Serial.print("occupancy = ");
   Serial.println(cha_occupancy.value.bool_value);  
 
-  //Update Threshold in EEPROM
+  //Update EEPROM
   if(cha_threshold.value.int_value != data.eepromThreshold){
     data.eepromThreshold = cha_threshold.value.int_value; //sync values
-    EEPROM.put(addr, data.eepromThreshold); //prepare changes
-    EEPROM.commit(); //Perform write to flash
+    EEPROM.put(addr, data.eepromThreshold);               //prepare changes, if any
+    EEPROM.commit();                                      //Perform write to flash
     Serial.print("Threshold changed - updated EEPROM to ");
     Serial.println(data.eepromThreshold); 
-  }
-
-}
-
-//This is what's run when you press identify during Homekit setup. For some reason, not when pressing identify once paired.
-void my_accessory_identify(homekit_value_t _value) {
-  printf("Identify Accessory\n");
-  for (int i = 0; i <= 5; i++) { //start at 0, run loop and add 1, repeat until 5
-    digitalWrite(LED_BUILTIN, LOW);// turn the LED on.(Note that LOW = LED on; this is because it is active low on the ESP8266.
-    delay(100);            // wait for 0.1 second.
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED off.
-    delay(100); // wait for 0.1 second.
   }
 }
--- my_accessory.c
+++ my_accessory.c
@@ -1,28 +1,10 @@
-/*
-   my_accessory.c
-   Define the accessory in C language using the Macro in characteristics.h
-
-  Based on the work by Mixiaoxiao (Wang Bin)
-*/
+//   my_accessory.c
+//   Define the accessory in C language using the Macro in characteristics.h
+//   Based on the work by Mixiaoxiao (Wang Bin) - https://github.com/Mixiaoxiao/Arduino-HomeKit-ESP8266
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 #include <Arduino.h>
-
-//This is what's run when you press identify during Homekit setup.
-void my_accessory_identify(homekit_value_t _value) {
-  printf("Identify Accessory\n");
-  for (int i = 0; i <= 5; i++) { //start at 0, run loop and add 1, repeat until 5
-    digitalWrite(LED_BUILTIN, LOW);// turn the LED on.(Note that LOW = LED on; this is because it is active low on the ESP8266.
-    delay(100);            // wait for 0.1 second.
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED off.
-    delay(100); // wait for 0.1 second.
-  }
-}
-
-// format: uint8; 0 ”Occupancy is not detected”, 1 ”Occupancy is detected”
-homekit_characteristic_t cha_occupancy = HOMEKIT_CHARACTERISTIC_(OCCUPANCY_DETECTED, 0);
-
 
 //Sensor Value. format: int32; 1 to 1024
 homekit_characteristic_t cha_sensorValue = HOMEKIT_CHARACTERISTIC_(CUSTOM,
@@ -33,7 +15,7 @@ homekit_characteristic_t cha_sensorValue = HOMEKIT_CHARACTERISTIC_(CUSTOM,
                  | homekit_permissions_notify,
     .min_value = (float[]) {0},
     .max_value = (float[]) {1024},
-    .min_step =  (float[]) {1},
+    .min_step =  (float[]) {1}
 );
 
 //Sensor Threshold. format: int32; 1 to 1024
@@ -47,6 +29,7 @@ homekit_characteristic_t cha_threshold = HOMEKIT_CHARACTERISTIC_(CUSTOM,
     .min_value = (float[]) {0},
     .max_value = (float[]) {1024},
     .min_step =  (float[]) {1},
+//    .value = HOMEKIT_INT_(50),
 );
 
 homekit_accessory_t *accessories[] = {
@@ -54,7 +37,7 @@ homekit_accessory_t *accessories[] = {
     HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
       HOMEKIT_CHARACTERISTIC(NAME, "Pressure Sensor Dev"),
       HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Dan Helmstedt"),
-      HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266 Analog"),
+      HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266 Analog Occupancy Sensor"),
       HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "12345678"),
       HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.8"),
       HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
@@ -62,6 +45,7 @@ homekit_accessory_t *accessories[] = {
     }),
     HOMEKIT_SERVICE(OCCUPANCY_SENSOR, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
       HOMEKIT_CHARACTERISTIC(NAME, "Pressure Sensor"),
+      HOMEKIT_CHARACTERISTIC(OCCUPANCY_DETECTED, 0),
       &cha_occupancy,
       &cha_sensorValue,
       &cha_threshold, 
@@ -77,3 +61,14 @@ homekit_server_config_t config = {
   .password = "120-41-997",
   .setupId = "1QJ8",
 };
+
+//Identify accessory by flashing LED 5x in 1 second
+void my_accessory_identify(homekit_value_t _value) {
+  printf("Identify Accessory\n");
+  for (int i = 0; i <= 5; i++) {      //start at 0, run loop and add 1, repeat until 5
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (In-built LED is active LOW)
+    delay(100);                       // wait for 0.1 second.
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED off.
+    delay(100);                       // wait for 0.1 second.
+  }
+}
